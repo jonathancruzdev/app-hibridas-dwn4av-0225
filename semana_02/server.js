47b509e06fbc23a542159c9d55c74b52e49a9930
Jonathan Cruz <cruzjonathan.e@gmail.com>
2025-08-21 20:46:43 -0300

    Semana 02 - Express

--- semana_02/server.js
+++ semana_02/server.js
@@ -1,4 +1,39 @@
 const Product = require('./Product.js');
 const chalk = require('chalk');
+const express = require('express');
+const port = 8080;
+
+const server = express();
+const model = new Product();
 console.log( chalk.green( 'API REST') );
-console.log( chalk.red( 'ERROR') );
+// console.log( chalk.red( 'ERROR') );
+
+const rutaRaiz = ( request, response ) => {
+    response.send('Hola desde Express 👋');
+}
+
+server.get( '/', rutaRaiz);
+
+server.get('/products', async ( request, response) => {
+    ///model.getProducts().then( list => { response.json(list); })
+    const list = await model.getProducts();
+    console.table(list);
+    response.json(list);
+})
+
+server.get('/products/:id', async ( request, response) => {
+    const id = request.params.id;
+    const data = await model.getProductById(id);
+    console.log(data);
+    response.json(data);
+})
+
+
+server.get('/users', ( request, response) => {
+    response.send('Lista de usuarios')
+})
+
+
+server.listen( port, () => {
+    console.log( chalk.green(`Servidor en el puerto ${port}`));
+})
